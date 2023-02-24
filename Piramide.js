5ff6a06159ecf40ea78db45c320e9c69799b0190
EnesT51 <99040669@mydavinci.nl>
2023-02-24 12:02:28 +0100

    opdr afgerond

--- Piramide.js
+++ Piramide.js
@@ -1,15 +1,29 @@
 function Piramide(){
+   var array = [];
+   var getal = prompt("Voer een Getal in: ");
+   for(let i = 0; i <= getal; i++){
+      array.push(i);
+   }
+   var title = document.createElement("h2");
+   var paragraph = document.createElement("p");
+   var para = document.createElement("p");
+   var node = document.createTextNode("Piramide Oplopend");
 
-   const getal = prompt("Voer een Getal in: ");
+   para.innerHTML = "Array: " + array;
+   title.appendChild(node);
+   document.body.appendChild(title);
+   document.body.appendChild(para);
+   document.body.appendChild(paragraph);
+   
    if(getal == null){
       alert("Input is leeg, voer een getal in.");
       Piramide();
    }
-   for(let i = 0; i < getal; i++){
-      for(let x = 0; x < i + 2; x++){
-         document.getElementById("Piramide").innerHTML += x;
+   for(let i = 0; i <= getal; i++){
+      for(let x = 0; x <= i; x++){
+         paragraph.innerHTML += array[x];
       }
-      document.getElementById("Piramide").innerHTML += "<br>";
+      paragraph.innerHTML += "<br>";
    }
 }
 Piramide();
